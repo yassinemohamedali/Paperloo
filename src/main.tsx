0af7802e53ed6a67999c08a1367cee56dac15b2b
yassinemohamedali <yassinemohamedali11@gmail.com>
2026-06-02 21:19:22 +0300

    chore: remove React Query Devtools
    Remove dev dependency to reduce bundle size and prevent accidental exposure of query state in production environments.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 import './index.css';
 
@@ -18,7 +17,6 @@ createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
